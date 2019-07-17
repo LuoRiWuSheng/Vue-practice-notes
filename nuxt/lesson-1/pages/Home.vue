477bbc017095db9d990fa3ec0717768d603d1fb8
luoriwusheng <1197802519@qq.com>
2019-07-17 17:37:33 +0800

    新增demo

---
+++ nuxt/lesson-1/pages/Home.vue
@@ -0,0 +1,7 @@
+<template>
+  <div>
+    首页
+    <nuxt-link to="/">首页</nuxt-link>
+    <nuxt-link to="/about">关于</nuxt-link>
+  </div>
+</template>
---
+++ vue/nextTick.js
@@ -0,0 +1,13 @@
+/*_this.$nextTick(function() {
+										var btnDomList = _this.$refs["recommand-container"].children
+										if(btnDomList.length > 0) {
+											if(_this.isShowSelectedBussiness === "visible") { // 表示选择了
+												Array.prototype.forEach.call(btnDomList, function(item, index) {
+													if(item.innerText === _this.jyfw) {
+														btnDomList[index].style.backgroundColor = "#999"
+														btnDomList[index].setAttribute("disabled", "disabled")
+													}
+												})
+											}
+										}
+									})*/
